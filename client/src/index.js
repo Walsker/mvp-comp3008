635c41e840582bb9f339038f85e2597aa802959a
Wal Wal <walskerw@gmail.com>
2020-03-25 15:19:48 -0400

    Create test page

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import { Home, Consent, Instructions, Scenario, Halfway, Completion } from 'pages'
+import { Home, Consent, Instructions, Scenario, Halfway, Completion, Test } from 'pages'
 import './index.css'
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/'>
-        <Completion />
+        <Test />
       </Route>
     </Switch>
   </BrowserRouter>
