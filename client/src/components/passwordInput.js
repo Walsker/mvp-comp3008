9a756f4d300f0fe64ff41c4b55980271337d2f12
Umai Balendra <umaibalendra@Umais-MacBook-Pro.local>
2020-04-08 14:14:18 -0400

    Co-authored-by: haohaodu <hdu2899@gmail.com>

--- client/src/components/passwordInput.js
+++ client/src/components/passwordInput.js
@@ -1,22 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import styles from './passwordInput.module.css'
 
+/* This file contains the code for our arrow password scheme. It keeps track 
+of which button on the arrow button pad has been clicked, and executes the
+respective button click event handlers. The button images were created through
+Figma and exported as .png files as an image on top of each HTML arrow button.
+*/
+
 const PasswordInput = ({ correctPassword, onCorrect, onIncorrect }) => {
+  //Declaring masterPassword as a current state variable, and 
+  //setMasterPassword is the function to update it (via a state hook)
   const [masterPassword, setMasterPassword] = useState('')
 
+  //Tells our component to check for correct password entry length each time 
+  //after masterPassword is re-rendered/updated
   useEffect(() => {
-    if (masterPassword.length === 7) {
-      if (masterPassword === correctPassword) {
+    //if they enter the correct length, check if entry is correct/incorrect 
+    //and call their respective functions
+    if (masterPassword.length === 7) { 
+      if (masterPassword === correctPassword) { 
         alert('Correct :)')
-        onCorrect()
+        onCorrect() 
       } else {
         alert('Incorrect :(')
         onIncorrect()
       }
-
-      setMasterPassword('')
+      //after the correct/incorrect function is called use state hook to reset masterPassword
+      setMasterPassword('') 
     }
-  }, [masterPassword])
+  }, [masterPassword]) //this part runs only when masterPassword is updated
+  //in this case, masterPassword is updated whenever the a button click occurs (seen below),
+  //rather than 
 
   //Button click event handlers
   const arrowClicked = number => () => setMasterPassword(masterPassword + number)
@@ -29,6 +43,7 @@ const PasswordInput = ({ correctPassword, onCorrect, onIncorrect }) => {
   const leftArrowClicked = arrowClicked('6')
   const upLeftArrowClicked = arrowClicked('7')
 
+  //Render buttons and images to screen
   return (
     <div className={styles.app}>
       <header className={styles.appHeader}>
